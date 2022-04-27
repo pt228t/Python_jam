ea1c0fc2256aa603f8ac27fcd02c265dc1954c4a
Pthakur <Pthakur@cvent.com>
2022-04-27 22:27:38 +0530

    1D and 2D Array in Data structure

---
+++ Data_structure_python/Array.py
@@ -0,0 +1,26 @@
+#Linear Data strucutre
+#Contiguous memory locations
+#Access elements randomly
+#Homogenus elements ie similar elements
+
+#Applications of array
+#- Storing information - Linear fashion
+#- Suitable for applications that requires frequent searching
+
+# 1-Dimensonal Array 
+# -Elements are stored one after another
+# -Only one subscript or index is used
+# -Can be related to one row
+
+#Array declaration:
+# Datatype varname[size];
+#int arr[5];
+# datatype varname[] = {1,2,3,4,5};
+# int arr[] = {1,2,3,4,5};
+
+
+#2D Array
+# It can be a table or matrix
+# Elements are stored one after another ie one 1D array inside other
+# Two subscripts or indices are used, one row and one column
+# Dimensions depends upon the number of subscripts used
